6c0465c2524204c04c9fea1e51208f42632a4b5b
uhCurious <lrichardson@webuy.com>
2026-04-29 16:00:52 +0100

    test

--- testing/scripts/iphone/app.js
+++ testing/scripts/iphone/app.js
@@ -1,4 +1,4 @@
-function showPage(page) {
+window.showPage = function(page) {
   document.querySelectorAll('.card').forEach(c => c.classList.add('hidden'));
   document.getElementById(page).classList.remove('hidden');
 }
